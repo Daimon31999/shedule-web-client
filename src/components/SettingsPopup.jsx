1b53ece5c1745ad7cfd29ee30fc377db13920f1e
Dima Hinev <dh@MacBook-Pro.local>
2020-12-06 20:00:25 +0200

    fixed on Iphone

--- src/components/SettingsPopup.jsx
+++ src/components/SettingsPopup.jsx
@@ -1,19 +1,19 @@
-import React, { useRef, useState, useEffect } from "react"
-import settingsImg from "../../static/assets/img/settings.png"
-import calendar_even from "./../img/calendar_even.png"
-import calendar_odd from "./../img/calendar_odd.png"
-import clock from "./../img/clock.png"
-import { Link } from "gatsby"
+import React, { useRef, useState, useEffect } from 'react'
+import settingsImg from '../../static/assets/img/settings.png'
+import calendar_even from './../img/calendar_even.png'
+import calendar_odd from './../img/calendar_odd.png'
+import clock from './../img/clock.png'
+import { Link } from 'gatsby'
 
 export default function SettingsPopup({ items, parity }) {
   const [isOpen, setIsOpen] = useState(false)
 
-  // useEffect(() => {
-  //   const timeout = setTimeout(() => {
-  //     setIsOpen(false)
-  //   }, 3000)
-  //   return () => clearTimeout(timeout)
-  // }, [isOpen])
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsOpen(false)
+    }, 3000)
+    return () => clearTimeout(timeout)
+  }, [isOpen])
 
   const toggle = () => {
     setIsOpen(!isOpen)
@@ -22,12 +22,11 @@ export default function SettingsPopup({ items, parity }) {
   return (
     <div>
       <div
-        className="w-10 h-10 lg:w-12 lg:h-12 mt-4 relative bg-cover cursor-pointer"
+        className='w-10 h-10 lg:w-12 lg:h-12 mt-4 relative bg-cover cursor-pointer'
         onClick={toggle}
         style={{
           backgroundImage: `url(${settingsImg})`,
-        }}
-      >
+        }}>
         {/* <div
           id="settings-modal"
           className={`${isOpen ? "block" : "hidden"}
@@ -35,31 +34,30 @@ export default function SettingsPopup({ items, parity }) {
           px-4 py-2 flex flex-row justify-center items-center`}
         > */}
         <div
-          className={`${isOpen ? "block" : "hidden"}
+          className={`${isOpen ? 'block' : 'hidden'}
           settings-modal
           absolute lg:left-14 lg:top-0 -left-14 top-12
           w-40 h-20 bg-front-blue rounded-2xl
-          px-4 py-2 flex flex-row justify-center items-center`}
-        >
-          <Link to="/calendar-admin" state={{ parity: "even" }}>
+          px-4 py-2 flex flex-row justify-center items-center`}>
+          <Link to='/calendar-admin' state={{ parity: 'even' }}>
             <img
-              className="hover-img-invert w-10 h-10 mr-3"
+              className='hover-img-invert w-10 h-10 mr-3'
               src={calendar_even}
-              alt=""
+              alt=''
             />
           </Link>
 
-          <Link to="/calendar-admin" state={{ parity: "odd" }}>
+          <Link to='/calendar-admin' state={{ parity: 'odd' }}>
             <img
-              className="hover-img-invert w-10 h-10 mr-3"
+              className='hover-img-invert w-10 h-10 mr-3'
               src={calendar_odd}
-              alt=""
+              alt=''
             />
           </Link>
 
           <br />
-          <Link to="/time-admin" state={{ parity }}>
-            <img className="hover-img-invert w-10 h-10" src={clock} alt="" />
+          <Link to='/time-admin' state={{ parity }}>
+            <img className='hover-img-invert w-10 h-10' src={clock} alt='' />
           </Link>
         </div>
       </div>
--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from "react"
-import "../../static/tailwind.css"
-import useInterval from "./../hooks/useInterval"
-import moment from "moment"
-import ReactTooltip from "react-tooltip"
+import React, { useState, useEffect } from 'react'
+import '../../static/tailwind.css'
+import useInterval from './../hooks/useInterval'
+import moment from 'moment'
+import ReactTooltip from 'react-tooltip'
 
 export default function Timer({
   timeTable,
@@ -19,18 +19,18 @@ export default function Timer({
   }, [])
 
   // TODO убрать повторение (в DayCard)
-  const getAbbreviation = sentence => {
+  const getAbbreviation = (sentence) => {
     if (sentence) {
       let result
-      sentence = sentence.split(" ")
-      result = sentence.map(word => word[0])
-      result = result.join("").toUpperCase()
+      sentence = sentence.split(' ')
+      result = sentence.map((word) => word[0])
+      result = result.join('').toUpperCase()
       return result
     }
   }
 
   const print = () => {
-    console.log("pair", pair)
+    console.log('pair', pair)
     // Формальные Языки Программирования
     if (pair) {
       // ФЯП
@@ -40,15 +40,15 @@ export default function Timer({
     // '' | undefined
     else if (breakIndex) {
       return (
-        <div className="text-4xl flex items-start">
-          <span>Перемена </span> <span className="ml-2">🥳</span>
+        <div className='text-4xl flex items-start'>
+          <span>Перемена </span> <span className='ml-2'>🥳</span>
         </div>
       )
     }
     // '' | undefined
     else {
       return (
-        <span className="text-4xl flex items-start">
+        <span className='text-4xl flex items-start'>
           <span>Иди домой</span> <span>🤟🥳</span>
         </span>
       )
@@ -56,9 +56,9 @@ export default function Timer({
   }
 
   const repeatingFunc = () => {
-    const whatPairIs = timeTable => {
-      let longFormat = "hh:mm:ss"
-      let shortFormat = "hh:mm"
+    const whatPairIs = (timeTable) => {
+      let longFormat = 'hh:mm:ss'
+      let shortFormat = 'hh:mm'
       let pairIndex
 
       let now = moment()
@@ -76,22 +76,22 @@ export default function Timer({
     let pairIndex = whatPairIs(timeTable)
     let currentPairTime = timeTable[pairIndex]
     if (currentPairTime) {
-      let shortFormat = "hh:mm"
+      let shortFormat = 'hh:mm'
       let secondsRemaining = moment
         .duration(moment(currentPairTime[1], shortFormat).diff(moment()))
         .asSeconds()
-      const result = moment.utc(secondsRemaining * 1000).format("HH:mm:ss")
+      const result = moment.utc(secondsRemaining * 1000).format('HH:mm:ss')
       setResultTime(result)
       let index = pairIndex
-      console.log("pairIndex", pairIndex)
-      console.log("currentPairTime", currentPairTime)
-      console.log("dayofweek", dayOfWeek)
-      console.log("index", index)
+      console.log('pairIndex', pairIndex)
+      console.log('currentPairTime', currentPairTime)
+      console.log('dayofweek', dayOfWeek)
+      console.log('index', index)
       let pairName
-      if (dayOfWeek === "воскресение") {
-        pairName = ": )"
+      if (dayOfWeek === 'воскресение') {
+        pairName = ': )'
       } else pairName = items[0].shedule[parity][dayOfWeek][index]
-      console.log("pariname", pairName)
+      console.log('pariname', pairName)
       setPair(pairName)
     }
   }
@@ -103,11 +103,10 @@ export default function Timer({
   return (
     // <div className="text-6xl font-bold text-blue flex flex-col justify-end ">
     <div
-      className="
-    text-5xl items-center lg:items-stretch -mt-16 lg:items
-    lg:text-6xl font-bold text-blue flex flex-col lg:justify-end "
-    >
-      <div className="flex items-center lg:justify-between">
+      className='
+    text-5xl items-center lg:items-stretch -mt-10 lg:-mt-16 lg:items
+    lg:text-6xl font-bold text-blue flex flex-col lg:justify-end '>
+      <div className='flex items-center lg:justify-between'>
         <span>
           {/* {getAbbreviation(pair) === undefined
             ? ""
@@ -115,34 +114,31 @@ export default function Timer({
 
           {print()}
         </span>
-        <a data-tip data-for="my_info">
+        <a data-tip data-for='my_info' data-event='click focus'>
           <svg
-            height="40"
-            width="40"
-            className={`${pair ? "" : "hidden"} ml-2 z-50 cursor-pointer`}
-          >
-            <circle cx="20" cy="20" r="20" fill="#017383" />
+            height='40'
+            width='40'
+            className={`${pair ? '' : 'hidden'} ml-2 z-50 cursor-pointer`}>
+            <circle cx='20' cy='20' r='20' fill='#017383' />
             <text
-              x="11"
-              y="31"
-              fontFamily="Verdana"
-              fontSize="30"
-              fill="#DDEAEA"
-            >
+              x='11'
+              y='31'
+              fontFamily='Verdana'
+              fontSize='30'
+              fill='#DDEAEA'>
               ?
             </text>
           </svg>
         </a>
 
         <ReactTooltip
-          id="my_info"
-          place="top"
-          clickable="true"
-          type="info"
-          effect="solid"
-          textColor="#fff"
-          backgroundColor="#017383"
-        >
+          id='my_info'
+          place='top'
+          clickable='true'
+          type='info'
+          effect='solid'
+          textColor='#fff'
+          backgroundColor='#017383'>
           <span>{pair}</span>
         </ReactTooltip>
       </div>
