f71d98975d2d4dd17ec87dd598348ac43ed0e9e1
Dima Hinev <dh@MacBook-Pro.local>
2020-12-10 19:18:34 +0200

    tailwind css imported

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
 import { Link } from 'gatsby'
 import LoginRegisterHeader from './../components/LoginRegisterHeader'
-
+import '../../static/tailwind.css'
+import '../css/App.css'
 import Axios from 'axios'
 
 export default function Register() {
